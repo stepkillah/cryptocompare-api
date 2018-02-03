5f13449dfe8e1db859a1a9176ba8df2038ba68ba
Anton Yaroshenko <kpakadijl@hotmail.com>
2018-02-03 15:36:27 +0200

    namespace fixed

--- src/CryptoCompare/Clients/NewsClient.cs
+++ src/CryptoCompare/Clients/NewsClient.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using CryptoCompareApi.News;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 
--- src/CryptoCompare/CryptoCompareClient.cs
+++ src/CryptoCompare/CryptoCompareClient.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using CryptoCompareApi.News;
 using JetBrains.Annotations;
 
 namespace CryptoCompare
